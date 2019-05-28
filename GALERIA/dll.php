6496b6d5d585ca10500eb0bc454d03f699e5b3ea
kasia330 <46824469+kasia330@users.noreply.github.com>
2019-05-28 20:12:21 +0200

    Create dll.php

---
+++ GALERIA/dll.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <form action="dl.php"   method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">	
+	  <button inptype type="submit" name="submit"> Usuń </button>
+	 </form>
+</body>
+
+
+</html>
